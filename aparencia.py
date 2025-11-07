88d52a8ebd1a3e4e4ddbedf8bcaaaa919394b8bf
Valessia Juliany <valessia.juliany@escolar.ifrn.edu.br>
2025-11-07 11:29:13 -0300

    Update aparencia.py

--- aparencia.py
+++ aparencia.py
@@ -10,6 +10,6 @@ CHAO = 385
 
 pygame.font.init()
 FONT_NAME = pygame.font.match_font('mono')
-
+FONT_TUTORIAL = pygame.font.match_font('arial')
 DIRETORIO_PRINCIPAL = os.path.dirname(__file__)
 DIRETORIO_IMAGENS = os.path.join(DIRETORIO_PRINCIPAL, "sprite")
