406e681dc23f50c0acf11608cf33e8050fe28dac
Valessia Juliany <valessia.juliany@escolar.ifrn.edu.br>
2026-02-01 07:21:26 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -1,7 +1,5 @@
 import pygame
-import os
-
-CHAO = 385
+from aparencia import CHAO
 
 class Personagem(pygame.sprite.Sprite):
     def __init__(self, spritesheet):
@@ -10,16 +8,19 @@ class Personagem(pygame.sprite.Sprite):
         self.andar_esquerda = []
         self.anim_morte = []
 
+        # Animação direita
         for i in range(4):
             img = spritesheet.subsurface((i * 85, 0), (85, 95))
             img = pygame.transform.scale(img, (85, 95))
             self.andar_direita.append(img)
 
+        # Animação esquerda
         for i in range(4, 8):
             img = spritesheet.subsurface((i * 85, 0), (85, 95))
             img = pygame.transform.scale(img, (85, 95))
             self.andar_esquerda.append(img)
 
+        # Animação morte
         for i in range(5, 8):
             img = spritesheet.subsurface((i * 85, 2 * 95), (85, 95))
             img = pygame.transform.scale(img, (85, 95))
@@ -35,6 +36,7 @@ class Personagem(pygame.sprite.Sprite):
         self.direcao = "direita"
         self.morrendo = False
         self.morto = False
+        self.na_plataforma = False
 
     def update(self):
         if self.morrendo:
@@ -51,6 +53,7 @@ class Personagem(pygame.sprite.Sprite):
                 self.image = self.anim_morte[int(self.index_lista)]
             return
 
+        # Animação andando
         if self.vel_x > 0:
             self.direcao = "direita"
             self.index_lista += 0.25
@@ -67,19 +70,30 @@ class Personagem(pygame.sprite.Sprite):
         else:
             self.image = self.andar_direita[0] if self.direcao == "direita" else self.andar_esquerda[0]
 
+        # Movimento horizontal
         self.rect.x += self.vel_x
-        self.vel_y += 1
-        self.rect.y += self.vel_y
+        if self.rect.left < 0:
+            self.rect.left = 0
 
-        if self.rect.bottom >= CHAO + 47:
-            self.rect.bottom = CHAO + 47
+        # Gravidade só se não estiver em plataforma
+        if not self.na_plataforma:
+            self.vel_y += 1
+            self.rect.y += self.vel_y
+        else:
             self.vel_y = 0
-            self.pulando = False
+
+        # Checa chão
+        if not self.na_plataforma and self.rect.bottom >= CHAO + 47:
+            self.rect.bottom = CHAO + 47
+            if self.vel_y > 0:
+                self.vel_y = 0
+                self.pulando = False
 
     def pular(self):
         if not self.pulando and not self.morrendo:
             self.vel_y = -20
             self.pulando = True
+            self.na_plataforma = False  # sai da plataforma ao pular
 
     def morrer(self):
         if not self.morrendo:
